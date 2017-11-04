9a919492148e889659c3972dcf81cd0f70fa1aaf
rangerazk <ansarazeemk@gmail.com>
2017-11-04 10:21:15 +0530

    hello world

---
+++ app.js
@@ -0,0 +1,15 @@
+function azk1(){
+console.log('boogeyman');
+
+console.log('asdsdff');
+console.log('new way');
+}
+function as(){
+console.log('asdf');
+
+console.log('bug fix');
+
+}
+console.log('hello world');
+azk1();
+as();
